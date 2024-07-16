e5e7d9773b618ae501e2b8d5e5dc0811a1a6e8f7
Aseef Khan <ak5488919@gmail.com>
2024-07-16 16:40:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgresql://aseef:dmABnCUras8QT6CgApZk4BtEXz1MQIGX@dpg-cqalhmggph6c73f61sc0-a:5432/asf')
     SECRET_KEY = os.getenv('SECRET_KEY', 'aseef_swati_parth_prakash_ninad')
