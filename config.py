ef146156b7cb195d67f488befef3306b2ab0e187
Aseef <ak5488919@gmail.com>
2024-07-16 14:02:11 +0530

    Fixed typo in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pysql://aptech:Aptech123@localhost/asf')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://aptech:Aptech123@localhost/asf')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'aseef_swati_parth_prakash_ninad')
 
