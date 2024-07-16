040251cc767dbdaddb654c26f0e6ea16ac6c47bb
Aseef <ak5488919@gmail.com>
2024-07-16 18:49:36 +0530

    xyz

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'aseef_swati_parth_prakash_ninad')
